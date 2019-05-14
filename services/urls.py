a173d4ff46936539f4da16cfd582de05b36cc36a
selmen1 <selmen.harhad@gmail.com>
2019-05-14 15:15:02 +0200

    ajouter le module d'inscription (register)

--- services/urls.py
+++ services/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView,LogoutView
 from .views import DashboardView
+from . import views
 
 urlpatterns = [
+	path('home',views.home,name='home'),
     path('login',view=LoginView.as_view(template_name="services/login.html",redirect_authenticated_user=True),name='login'),
     path('logout',view=LogoutView.as_view(),name='logout'),
     path('dashboard',view=DashboardView.as_view(),name='dashboard'),
+    path('register',views.register,name='register'),
 
 ]
--- services/views.py
+++ services/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+
+def home(request):
+	return render(request,'services/home.html')
 
 class DashboardView(TemplateView):
     """docstring for DashboardView"""
     template_name='services/dashboard.html'
+
+def register(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid() :
+			form.save()
+			return redirect('login')
+
+	else:
+		form=UserCreationForm()
+
+
+	args={'form':form}
+	return render(request,'services/reg_form.html',args)		
